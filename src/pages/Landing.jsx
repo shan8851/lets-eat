59eeb05d9141d56cfaa8abdb36ac1fdd4ee9ce2c
shan5742 <asamshan456@gmail.com>
2021-07-14 06:43:34 +0100

    adding social links to landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,6 +5,12 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import { Link } from "react-router-dom";
 import { colors } from "../helpers/globals";
+import {
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillLinkedin,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 
 export const Landing = () => {
   const url =
@@ -62,6 +68,39 @@ export const Landing = () => {
         />
       )}
       {signedUp && <Success>Thank for signing up, we'll be in touch.</Success>}
+      <Row>
+        <SocialBlock>
+          FIND US ON |{" "}
+          <SocialLink
+            href="https://www.facebook.com/uk.lets.eat"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillFacebook size={22} />
+          </SocialLink>{" "}
+          <SocialLink
+            href="https://www.linkedin.com/company/lets-eat-uk/?viewAsMember=true"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillLinkedin size={22} />
+          </SocialLink>{" "}
+          <SocialLink
+            href="https://twitter.com/Lets_Eat_UK"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineTwitter size={22} />
+          </SocialLink>{" "}
+          <SocialLink
+            href="https://www.instagram.com/lets.eat.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineInstagram size={22} />
+          </SocialLink>{" "}
+        </SocialBlock>
+      </Row>
     </Container>
   );
 };
@@ -149,4 +188,25 @@ const NavLink = styled(Link)`
   color: ${colors.yellow};
   font-weight: 800;
   padding: 10px;
+`;
+
+const SocialBlock = styled.p`
+  margin: 0 0 0 8px;
+  font-size: 15px;
+  font-weight: bold;
+  text-decoration: none;
+  color: ${colors.yellow};
+  display: flex;
+  align-items: center;
+`;
+
+const SocialLink = styled.a`
+  color: ${colors.yellow};
+  margin: 0;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 16px 0;
 `;
